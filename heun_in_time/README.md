51c2a4db8b04e26b66451d9303247146506da9f3
bryceshirley <bryce.shirley@stfc.ac.uk>
2026-03-02 17:45:14 +0000

    refactor: simulation now saves configuration and plot into a results dir

--- heun_in_time/README.md
+++ heun_in_time/README.md
@@ -7,9 +7,11 @@ uv sync
 
 ## Run Simulation
 
-Run the following command to start the simulation
+Run the following command to start the simulation from heun_in_time/ directory
 ```bash
 uv run simulation.py
 ```
 
-Saves into Advection.png
+Saves into results/{timestamp} which contains the following files:
+- Advection.png: The plot of the solution at the final time step.
+- configuration.txt: A text file containing the simulation parameters used for this run.
--- heun_in_time/simulation.py
+++ heun_in_time/simulation.py
@@ -1,28 +1,68 @@
+import os
 import time
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+# -------------------------
+# Results Directory
+# -------------------------
+def create_results_dir():
+    """
+    Creates a time stamped results directory.
+    """
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    results_dir = f"results/{timestamp}"
+    os.makedirs(results_dir, exist_ok=True)
+    return results_dir
+
+# Create Results Directory
+RESULTS_DIR = create_results_dir()
+
+
+# -------------------------
 # Helper Functions
-def plotsol_animate(u_n, i):
+# -------------------------
+
+def plotsol_animate(u_n, i, delta_t, sleep_time=0.1, results_dir=RESULTS_DIR):
+    """
+    Plots the solution at each time step for animation.
+    Parameters:
+    - u_n: The solution at the current time step.
+    - i: The current time step index.
+    - delta_t: The time step size.
+    """
+    if results_dir is None:
+        results_dir = create_results_dir()
+    # Plot the solution
     plt.plot(range(len(u_n)), u_n)
     plt.xlabel("t")
     plt.ylabel("u")
-    plt.title(f"Solution of the Advection Equation, t = {i}")
-    plt.savefig("Advection.png")
-    time.sleep(0.1)
+    plt.title(f"Solution of the Advection Equation, t = {i * delta_t}")
+    plt.savefig(f"{results_dir}/Advection.png")
+    time.sleep(sleep_time)
     plt.close()
 
 
-def plotsol(u_n, i):
+def plotsol(u_n, i, delta_t):
+    """
+    Plots the solution at a time step.
+    Parameters:
+    - u_n: The solution at the current time step.
+    - i: The current time step index.
+    - delta_t: The time step size.
+    """
     plt.plot(range(len(u_n)), u_n)
     plt.xlabel("t")
     plt.ylabel("u")
-    plt.title(f"Solution of the Advection Equation, t = {i}")
+    plt.title(f"Solution of the Advection Equation, t = {i * delta_t}")
     plt.show()
     plt.close()
 
+# ----------------------------
+# Heun's Method Implementation
+# ----------------------------
 def compute_u_hat(u_n: np.ndarray, c: float) -> np.ndarray:
     """
     Computes the intermediate variable u_hat.
@@ -51,25 +91,42 @@ def update_u_n(u_n: np.ndarray, c: float) -> np.ndarray:
     return -(c / 4) * u_n + (1 + c / 4) * np.roll(u_n, 1) + u_hat
 
 
-def solve(u_n: np.ndarray, c: float, N: int, animate: bool = False) -> np.ndarray:
+def solve(u_n: np.ndarray, c: float, N: int, delta_t: float, 
+          animate: bool = False, 
+          sleep_time: float = 0.1) -> np.ndarray:
     """
     Solves the advection equation for N time steps.
     """
     for i in range(N):
         u_n = update_u_n(u_n, c)
         if animate:
-            plotsol_animate(u_n, i)
+            plotsol_animate(u_n, i, delta_t, sleep_time=sleep_time)
     return u_n
 
 
+# -------------------
+# Main Simulation
+# ------------------
+
 if __name__ == "__main__":
+    # -------------------------
     # Simulation Parameters
-    delta_t = 0.5
-    M = 10
-    a = 1
+    # -------------------------
+
+    # Advection Speed
+    a = 3.0
+
+    # Space Discretisation
+    M = 100
     L = 100
     delta_x = L / M
-    N = int(L / delta_t)
+
+    # Time Discretisation
+    delta_t = 0.5
+    tmax = 10
+    N = int(tmax / delta_t) # Number of time steps
+
+    # Courant Number
     c = (a * delta_x) / delta_t
 
     # Initial Condition
@@ -77,7 +134,14 @@ if __name__ == "__main__":
     u_n[: (M // 2)] = 0
 
     # Solve the PDE
-    u_n = solve(u_n, c, N, animate=True)
+    u_n = solve(u_n, c, N, delta_t, animate=True, sleep_time=0.1)
 
-    # Plot the final solution
-    plotsol(u_n, N)
+    # Dump Configuration in txt file
+    with open(f"{RESULTS_DIR}/configuration.txt", "w") as f:
+        if a is not None:
+            f.write(f"Advection Speed: {a}\n")
+        if M is not None and L is not None and delta_x is not None:
+            f.write(f"Space Discretisation: M = {M}, L = {L}, delta_x = {delta_x}\n")
+        if delta_t is not None and tmax is not None and N is not None:
+            f.write(f"Time Discretisation: delta_t = {delta_t}, tmax = {tmax}, N = {N}\n")
+        f.write(f"Courant Number: c = {c}\n")
