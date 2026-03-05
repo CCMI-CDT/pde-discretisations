55b89fb4ab44e1f6fe77d6521a23637383d46761
Kelan <k.gray25@imperial.ac.uk>
2026-03-05 14:25:53 +0000

    Add error plot over basis functions

--- schrodinger/schrodinger_errors.py
+++ schrodinger/schrodinger_errors.py
@@ -1,58 +1,72 @@
 from firedrake import *
 import matplotlib.pyplot as plt
 
+# Time derivative
 def dudt(u, u_, dt):
     return ((u - u_)/dt)
 
-num_cells = 100 # Number of cells in each direction for the mesh
-checkpointing = False # Checkpointing flag
-t_max = 0.3
-dt = 1/num_cells
+t_max = 0.1
+dt = 1/1000 # Time discretisation large enough to have negligible effect on error
 
-k = 2 * pi
+k = 2 * pi # Frequency 
 omega = k**2
 
-# Create a unit square mesh
-mesh = PeriodicIntervalMesh(num_cells, 1.0)
-x = SpatialCoordinate(mesh)
+cells = np.arange(5, 50) # Increasing set of basis functions
+final_errors = []
 
-V = VectorFunctionSpace(mesh, "CG", 1, dim=2)
+for num_cells in cells: 
+    mesh = PeriodicIntervalMesh(num_cells, 1.0) # Periodic domain (1D)
+    x = SpatialCoordinate(mesh)
 
-v = TestFunction(V)
+    V = VectorFunctionSpace(mesh, "CG", 1, dim=2) # Complex plane as \R^2
 
-psi_0 = Function(V)
-psi_0.interpolate(as_vector([cos(k*x[0]), sin(k*x[0])]))
-psi_r0, psi_i0 = split(psi_0)
+    v = TestFunction(V)
 
-psi = Function(V)
-psi.assign(psi_0)
-psi_r, psi_i = split(psi)
+    psi_0 = Function(V)
+    psi_0.interpolate(as_vector([cos(k*x[0]), sin(k*x[0])]))
+    psi_r0, psi_i0 = split(psi_0)
 
-F1 = (inner(dudt(psi_i, psi_i0, dt), v[0]) + inner(grad(psi_r), grad(v[0]))) * dx
-F2 = (inner(dudt(psi_r, psi_r0, dt), v[1]) - inner(grad(psi_i), grad(v[1]))) * dx
-F = F1 + F2
+    psi = Function(V)
+    psi.assign(psi_0)
+    psi_r, psi_i = split(psi) # Split into real and complex parts
 
-# step through time and record solution
-t = 0.0
-t_ufl = Constant(0.0)                 
-psi_true = Function(V, name="psi_true")
+    # Residual in real and complex variables
+    F1 = (inner(dudt(psi_i, psi_i0, dt), v[0]) + inner(grad(psi_r), grad(v[0]))) * dx
+    F2 = (inner(dudt(psi_r, psi_r0, dt), v[1]) - inner(grad(psi_i), grad(v[1]))) * dx
+    F = F1 + F2
 
-times = []
-l2_errors = []
+    t = 0.0
+    t_ufl = Constant(0.0)                 
+    psi_true = Function(V, name="psi_true")
 
-while t < t_max - dt/2:
-    t += dt
-    solve(F == 0, psi)
-    psi_0.assign(psi)
+    times = []
+    l2_errors = []
+
+    # step through time and record solution
+    while t < t_max - dt/2:
+        t += dt
+        solve(F == 0, psi)
+        psi_0.assign(psi)
+
+        t_ufl.assign(t)
+
+        #True solution 
+        psi_true.interpolate(as_vector([cos(k * x[0] -  omega * t_ufl), sin(k * x[0] -  omega *  t_ufl)]))
+
+        err_l2 = norms.errornorm(psi, psi_true) # L2 error 
+
+        times.append(t)
+        l2_errors.append(err_l2)
+    
+    # Only return final error
+    final_errors.append(l2_errors[-1])
+
+final_errors = np.array(final_errors)
 
 
-    t_ufl.assign(t)
-    psi_true.interpolate(as_vector([cos(k * x[0] -  omega * t_ufl), sin(k * x[0] -  omega *  t_ufl)]))
-
-    err_l2 = norms.errornorm(psi, psi_true)
-
-    times.append(t)
-    l2_errors.append(err_l2)
-
-plt.plot(times, l2_errors)
+plt.figure()
+plt.plot(cells, final_errors, '-', label='L2 error')
+plt.xlabel('Number of cells')
+plt.ylabel('Final time L2 error')
+plt.legend()
 plt.show()
